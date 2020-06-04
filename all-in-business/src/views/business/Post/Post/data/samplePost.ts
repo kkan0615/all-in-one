dea52d43f652fdec55dca39b6bdff0db1b004073
Youngjin Kwak <46660361+kkan0615@users.noreply.github.com>
2020-06-04 13:50:18 +0900

    Post get

---
+++ all-in-business/src/views/business/Post/Post/data/samplePost.ts
@@ -0,0 +1,5 @@
+export const samplePost =
+`
+<h1>It is test tilte</h1>
+<p>What happen to here? Can you expect?</p>
+`
--- all-in-business/src/views/business/Post/Post/index.vue
+++ all-in-business/src/views/business/Post/Post/index.vue
@@ -1,16 +1,37 @@
 <template>
-  <div>
-    test
-  </div>
+  <div
+    v-html="post"
+  />
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import { samplePost } from './data/samplePost'
 
 @Component({
   name: 'BusinessPost'
 })
 export default class extends Vue {
+  private post ?: string
+  private menus ?: Array<any>
+
+  constructor() {
+    super()
+    this.post = samplePost
+  }
+
+  created() {
+    this.createMenu()
+  }
+
+  private createMenu() {
+    const titleTags = document.body.getElementsByTagName('h1')
+    console.log(titleTags)
+    for (let i = 0; i < titleTags.length; i++) {
+      const element = titleTags[i]
+      element.setAttribute('id', `${i}h1`)
+    }
+  }
 }
 </script>
 
