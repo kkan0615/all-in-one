3fd4255c14c36a111f31eebfc5fcd2ab9b96512c
kkan0615 <kkan0615@gmail.com>
2020-06-09 23:21:51 +0900

    update

---
+++ all-in-business/src/views/business/Restaurant/Tables/components/setting.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    setting
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'BusinessRestaurnatTablesSetting'
+
+})
+export default class extends Vue {
+}
+</script>
+
+<style scoped>
+</style>
---
+++ all-in-business/src/views/business/Restaurant/Tables/components/table.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    table in Component
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'BusinessRestaurnatTables'
+
+})
+export default class extends Vue {
+}
+</script>
+
+<style scoped>
+</style>
