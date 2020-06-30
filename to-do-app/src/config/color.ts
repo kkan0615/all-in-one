ba329a676869a18ef21a9d59baeb6a6f8668eaf6
kkan0615 <kkan0615@gmail.com>
2020-06-30 18:26:43 +0900

    Update

---
+++ to-do-app/src/config/color.ts
@@ -0,0 +1,15 @@
+import { VuetifyThemeVariant } from 'vuetify/types/services/theme'
+
+export interface ColorConfigInterface {
+  isDark: boolean
+  VuetifyThemeVariant: VuetifyThemeVariant
+}
+
+// export const colorList :Array<ColorConfigInterface> = [
+//     {
+//         isDark: true
+//         VuetifyThemeVariant: {
+
+//         }
+//     }
+// ]
