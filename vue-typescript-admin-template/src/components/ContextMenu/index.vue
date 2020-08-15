25f88422eee26846173e7eb50d59106f2e06dc91
kkan0615 <kkan0615@gmail.com>
2020-08-16 00:09:47 +0900

    Add: ContextMenu

---
+++ vue-typescript-admin-template/src/components/ContextMenu/index.vue
@@ -0,0 +1,43 @@
+<!--
+  Author: Youngjin Kwak
+  CreatedAt: 08-15-2020
+  UpdatedAt: 08-15-2020
+  Description: ContextMenu global Component
+-->
+<template>
+  <div
+    @contextmenu.prevent="onRightClick"
+  >
+    <slot name="content" />
+    <v-menu
+      v-model="show"
+      top
+      :position-x="x"
+      :position-y="y"
+      absolute
+      offset-y
+    >
+      <slot name="menu" />
+    </v-menu>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'ContextMenu',
+})
+export default class ContextMenu extends Vue {
+  private show = false
+  private x = 0
+  private y = 0
+
+  private onRightClick (e: MouseEvent) {
+    this.show = false
+    this.x = e.clientX
+    this.y = e.clientY
+    this.show = true
+  }
+}
+</script>
